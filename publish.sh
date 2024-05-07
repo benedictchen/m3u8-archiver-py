cb9d22716d1a258dfca41aa5a692f7005b74e0ec
Benedict Chen <benedict@benedictchen.com>
2024-05-07 10:45:59 +0900

    Add update.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,9 @@
 ./start.sh
+python -m pip install pip-tools
+python -m pip install build twine bumpver
 python -m build
 pip-compile --extra dev pyproject.toml
 pip-sync
 bumpver update --minor
-twine check dist/* && twine upload dist/*
+twine check dist/*
+twine upload dist/*  --skip-existing --verbose
